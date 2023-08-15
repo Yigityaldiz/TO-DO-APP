ea31a4f294f7669e6f34ea832ab4d9fdb7be0d90
Yigit Yaldiz <yigityaldiz@gmail.com>
2023-08-15 23:27:08 +0300

    Build vite

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <>
+        <div id="mainpage" className=" bg-slate-900 h-screen w-screen flex items-center">
+
+
+
+        <form action="">
+            <input type="text" />
+            <input type="button" />
+        </form>
+
+        </div>
+       
+      </>
+    );
+  }
+}
